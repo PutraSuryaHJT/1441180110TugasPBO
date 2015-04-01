75fb0e9f75aa2d289a82a14070ede28531909d9e
PutraSuryaHJT <putsur@gmail.com>
2015-03-31 22:52:39 -0700

    Revert "Revert "lanjutan""
    This reverts commit da00a9b8eddfe9cf22dd2f2e4a1d589adcec6be1.

---
+++ Tugas 2/no5.java
@@ -0,0 +1,17 @@
+class no5 {
+	public static void main(String[] args) {
+		// deretan angka
+		// input 1 dan 10
+		int i;
+		for (i=1; i<=10; i++) 
+		{
+			System.out.print(i);
+			
+			if (i < 10)
+			{
+				System.out.print(",");
+			}
+		}
+
+	}
+}
